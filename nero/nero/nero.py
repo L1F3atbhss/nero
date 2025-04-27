deb4d15c57e0b5c06e45b3fd046ebc8070c715fd
L1F3atbhss <Nathan.chan3000@gmail.com>
2025-04-27 10:52:19 -0700

    Update nero.py

--- nero/nero/nero.py
+++ nero/nero/nero.py
@@ -1,7 +1,11 @@
 import os
 import nmap
 
+def get_ipa():
+    result = os.popen("ip route show default").read()
+    gateway = result.split()[2]
+    return gateway
+
+#print(get_default_gateway())
 
 NM = nmap.PortScaner()
-
-NM = nmap.scan(ipa)
