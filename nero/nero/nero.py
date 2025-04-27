42abe3cb09586d66c97fd9d5ebc3cbfb057990cf
L1F3atbhss <Nathan.chan3000@gmail.com>
2025-04-27 11:09:08 -0700

    Update nero.py

--- nero/nero/nero.py
+++ nero/nero/nero.py
@@ -15,6 +15,17 @@ def get_router_ip():
         return parts[2] if "default" in parts else None
 
 router_ip = get_router_ip()
-print(router_ip)
+print(f"Router IP: {router_ip}")
 
-NM = nmap.PortScaner()
+nm = nmap.PortScanner()
+
+# Scan the router
+nm.scan(router_ip, arguments='-sV -Pn -T4')  # -sV = detect service version
+
+# Print open ports with service info
+for proto in nm[router_ip].all_protocols():
+    ports = nm[router_ip][proto].keys()
+    for port in ports:
+        service = nm[router_ip][proto][port]['name']
+        state = nm[router_ip][proto][port]['state']
+        print(f"Port {port}/{proto} is {state}, service: {service}")
