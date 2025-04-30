6cd2899d303065c9c1591d3f56c45aa35e7803fb
L1F3atbhss <Nathan.chan3000@gmail.com>
2025-04-30 17:20:20 -0400

    .

--- nero/nero/nero.py
+++ nero/nero/nero.py
@@ -1,31 +1,21 @@
 import os
-import nmap
-import platform
 import re
 
-def get_router_ip():
-    system = platform.system()
-    if system == "Windows":
-        output = os.popen("ipconfig").read()
-        match = re.search(r"Default Gateway.+?: (\d+\.\d+\.\d+\.\d+)", output)
-        return match.group(1) if match else None
-    else:
-        output = os.popen("ip route show default").read()
-        parts = output.split()
-        return parts[2] if "default" in parts else None
+#var = input
+#ip address var
 
-router_ip = get_router_ip()
-print(f"Router IP: {router_ip}")
+def banner():
+    print("""
+  N   N  EEEEE  RRRR    OOO  
+  NN  N  E      R   R  O   O 
+  N N N  EEEE   RRRR   O   O 
+  N  NN  E      R  R   O   O 
+  N   N  EEEEE  R   R   OOO  
+    """)
+    print("Welcome to Project NERO")
+    print("Network Enumeration Rapid Operations\n")
 
-nm = nmap.PortScanner()
+def main():
+    banner()
 
-# Scan the router
-nm.scan(router_ip, arguments='-sV -Pn -T4')  # -sV = detect service version
-
-# Print open ports with service info
-for proto in nm[router_ip].all_protocols():
-    ports = nm[router_ip][proto].keys()
-    for port in ports:
-        service = nm[router_ip][proto][port]['name']
-        state = nm[router_ip][proto][port]['state']
-        print(f"Port {port}/{proto} is {state}, service: {service}")
+main()
